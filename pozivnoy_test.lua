de5801b0532dea3f4dfc11cadf5eb04a1ad24473
Enotiwe <enotyaone@gmail.com>
2022-06-02 19:50:03 +0300

    Update pozivnoy_test.lua

--- pozivnoy_test.lua
+++ pozivnoy_test.lua
@@ -1,6 +1,6 @@
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(0.7) 						            -- Указываем версию скрипта / FINAL
+script_version(1.1) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
