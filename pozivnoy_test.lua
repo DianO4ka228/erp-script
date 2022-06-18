432a423165f102ab613e68e562e63ca2c1374025
Enotiwe <enotyaone@gmail.com>
2022-06-18 21:26:57 +0300

    upd

--- pozivnoy_test.lua
+++ pozivnoy_test.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывной Наташи.
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(2.60) 						            -- Указываем версию скрипта / FINAL
+script_version(2.61) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -46,6 +46,7 @@ local nicks = { -- [''] = '',
   ['Foxit_Makayonok'] = 'Лис',
   ['Rayn_Pember'] = 'Белый',
   ['Leonardo_Defoe'] = 'Топор',
+  ['Sonny_Clarck'] = 'Гриф',
   
 -- Резерв
   ['Mia_Lightwood'] = 'Док',
