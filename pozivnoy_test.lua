bb07b01a91c134c1886820a76a15d1c2957ca029
RREENNSSHHII <108368129+RREENNSSHHII@users.noreply.github.com>
2022-07-14 11:18:31 +0300

    upd 14/07 [2]

--- pozivnoy_test.lua
+++ pozivnoy_test.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(2.86) 						            -- Указываем версию скрипта / FINAL
+script_version(2.87) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -52,6 +52,8 @@ local nicks = { -- [''] = '',
   ['Siegmund_Berg'] = 'Зодд',
   ['Aiden_Florestino'] = 'Призрак',
   ['Anthony_Diez'] = 'Медведь',
+  ['Sergey_Fibo'] = 'Панда',
+  ['Kwenyt_Hokage'] = 'Скорпион',
   -- [''] = '',
   -- [''] = '',
   -- [''] = '',
