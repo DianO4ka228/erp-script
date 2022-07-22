92f34cc5bd5f011f9455dc2b048087fed96dc9d0
RREENNSSHHII <108368129+RREENNSSHHII@users.noreply.github.com>
2022-07-22 20:15:39 +0300

    upd 22/07 [2]

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(2.99) 						            -- Указываем версию скрипта / FINAL
+script_version(3.00) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -54,6 +54,7 @@ local nicks = { -- [''] = '',
   
 -- Стажеры.
   ['Comtonia_Oceguera'] = 'Компот',
+  ['Makar_Ryabov'] = 'Чех',
   ['Dini_Raksize'] = 'Дино'
 }
 
