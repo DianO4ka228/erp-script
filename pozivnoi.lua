d1bba12efc71c1eeab30fb6226a265f0e050f41d
DianO4kaYa <111657769+DianO4ka228@users.noreply.github.com>
2022-08-26 11:52:08 +0300

    Update pozivnoi.lua

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(3.27) 						            -- Указываем версию скрипта / FINAL
+script_version(3.28) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
