5fd45f9cb3aa1d767c3e8bdf032bdf19ded624e9
DianO4kaYa <111657769+DianO4ka228@users.noreply.github.com>
2022-08-30 22:17:43 +0300

    Add files via upload

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(3.40) 						            -- Указываем версию скрипта / FINAL
+script_version(3.41) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -25,7 +25,7 @@ local nicks = { -- [''] = '',
   ['Blayzex_Stoun'] = 'Джамбо', -- Командир.
   ['Suetlan_Zelimxanov'] = 'Суета', -- Зам. Командира.
   ['Sky_Sillence'] = 'Таеро', -- Инструктор.
-  [''] = '', -- Инструктор.
+  ['Sergey_Fibo'] = 'Панда', -- Инструктор.
   [''] = '', -- Инструктор.
   
 -- Бойцы.
