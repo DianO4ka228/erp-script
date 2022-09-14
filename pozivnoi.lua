9adb8eec27bf550327a6a5c2f3e1620b56050fc2
fibo2022 <112514178+fibo2022@users.noreply.github.com>
2022-09-14 20:16:50 +0300

    Add files via upload

--- pozivnoi.lua
+++ pozivnoi.lua
@@ -1,7 +1,7 @@
 --Update: Обновил позывные/состав
 -- Информация о скрипте
 script_name('«Auto-Doklad»') 		                    -- Указываем имя скрипта
-script_version(3.64) 						            -- Указываем версию скрипта / FINAL
+script_version(3.65) 						            -- Указываем версию скрипта / FINAL
 script_author('Henrich_Rogge', 'Marshall_Milford', 'Andy_Fawkess') 	-- Указываем имя автора
 
 -- Библиотеки
@@ -50,6 +50,7 @@ local nicks = { -- [''] = '',
   ['Anthony_Carrios'] = 'Джин',
   ['Foxit_Makayonok'] = 'Лис',
   ['Valera_Milen'] = 'Цветок',
+  ['Suetlan_Zelimxanov'] = 'Суэта',
   ['Shredder_Rose'] = 'Роза'
   
 }
